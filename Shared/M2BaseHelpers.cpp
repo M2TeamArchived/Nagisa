7483d461549168efaba0dd88c87979142e35b2b9
Naruto Mouri <Mouri_Naruto@outlook.com>
2019-02-04 03:30:30 +0800

    Update M2BaseHelpers from NSudo.

--- Shared/M2BaseHelpers.cpp
+++ Shared/M2BaseHelpers.cpp
@@ -1,13 +1,14 @@
-﻿/******************************************************************************
-Project: M2-Team Common Library
-Description: Implementation for the basic helper functions.
-File Name: M2BaseHelpers.cpp
-License: The MIT License
-******************************************************************************/
+﻿/*
+ * PROJECT:   M2-Team Common Library
+ * FILE:      M2BaseHelpers.cpp
+ * PURPOSE:   Implementation for the basic helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
 
 #include "pch.h"
-
-#ifdef _M2_BASE_HELPERS_
 
 #include <Windows.h>
 
@@ -15,18 +16,13 @@ License: The MIT License
 
 #include <string>
 
-/// <summary>
-/// Write formatted data to a string.
-/// </summary>
-/// <param name="Format">
-/// Format-control string.
-/// </param>
-/// <param name="...">
-/// Optional arguments to be formatted.
-/// </param>
-/// <returns>
-/// Returns a formatted string if successful, or "N/A" otherwise.
-/// </returns>
+/**
+ * Write formatted data to a string.
+ *
+ * @param Format Format-control string.
+ * @param ... Optional arguments to be formatted.
+ * @return A formatted string if successful, "N/A" otherwise.
+ */
 std::wstring M2FormatString(
     _In_z_ _Printf_format_string_ wchar_t const* const Format,
     ...)
@@ -65,13 +61,12 @@ std::wstring M2FormatString(
     return L"N/A";
 }
 
-/// <summary>
-/// Retrieves the number of milliseconds that have elapsed since the system was
-/// started.
-/// </summary>
-/// <returns>
-/// The number of milliseconds.
-/// </returns>
+/**
+ * Retrieves the number of milliseconds that have elapsed since the system was
+ * started.
+ *
+ * @return The number of milliseconds.
+ */
 ULONGLONG M2GetTickCount()
 {
     LARGE_INTEGER Frequency = { 0 }, PerformanceCount = { 0 };
@@ -87,15 +82,12 @@ ULONGLONG M2GetTickCount()
     return GetTickCount64();
 }
 
-/// <summary>
-/// Converts from the UTF-8 string to the UTF-16 string.
-/// </summary>
-/// <param name="UTF8String">
-/// The UTF-8 string you want to convert.
-/// </param>
-/// <returns>
-/// The return value is the UTF-16 string.
-/// </returns>
+/**
+ * Converts from the UTF-8 string to the UTF-16 string.
+ *
+ * @param UTF8String The UTF-8 string you want to convert.
+ * @return A converted UTF-16 string.
+ */
 std::wstring M2MakeUTF16String(const std::string& UTF8String)
 {
     std::wstring UTF16String;
@@ -122,15 +114,12 @@ std::wstring M2MakeUTF16String(const std::string& UTF8String)
     return UTF16String;
 }
 
-/// <summary>
-/// Converts from the UTF-16 string to the UTF-8 string.
-/// </summary>
-/// <param name="UTF16String">
-/// The UTF-16 string you want to convert.
-/// </param>
-/// <returns>
-/// The return value is the UTF-8 string.
-/// </returns>
+/**
+ * Converts from the UTF-16 string to the UTF-8 string.
+ *
+ * @param UTF16String The UTF-16 string you want to convert.
+ * @return A converted UTF-8 string.
+ */
 std::string M2MakeUTF8String(const std::wstring& UTF16String)
 {
     std::string UTF8String;
@@ -161,60 +150,77 @@ std::string M2MakeUTF8String(const std::wstring& UTF16String)
     return UTF8String;
 }
 
-/// <summary>
-/// Retrieves the calling thread's last-error code value. The last-error code is
-/// maintained on a per-thread basis. Multiple threads do not overwrite each
-/// other's last-error code.
-/// </summary>
-/// <returns>
-/// The return value is the calling thread's last-error code which is converted
-/// to an HRESULT value.
-/// </returns>
+/**
+ * Retrieves the calling thread's last-error code value. The last-error code is
+ * maintained on a per-thread basis. Multiple threads do not overwrite each
+ * other's last-error code.
+ *
+ * @return The calling thread's last-error code which is converted to an
+ *         HRESULT value.
+ */
 HRESULT M2GetLastError()
 {
     return __HRESULT_FROM_WIN32(GetLastError());
 }
 
-/// <summary>
-/// Retrieves the address of an exported function or variable from the specified
-/// dynamic-link library (DLL).
-/// </summary>
-/// <param name="lpProcAddress">
-/// The address of the exported function or variable.
-/// </param>
-/// <param name="hModule">
-/// A handle to the DLL module that contains the function or variable. The
-/// LoadLibrary, LoadLibraryEx, LoadPackagedLibrary, or GetModuleHandle function
-/// returns this handle. This function does not retrieve addresses from modules
-/// that were loaded using the LOAD_LIBRARY_AS_DATAFILE flag. For more
-/// information, see LoadLibraryEx.
-/// </param>
-/// <param name="lpProcName">
-/// The function or variable name, or the function's ordinal value. If this
-/// parameter is an ordinal value, it must be in the low-order word; the
-/// high-order word must be zero.
-/// </param>
-/// <returns>
-/// The function will return HRESULT. If the function succeeds, the return value
-/// is S_OK.
-/// </returns>
+/**
+ * Retrieves the calling thread's last-error code value if you can be sure that
+ * the last call was failed. The last-error code is maintained on a per-thread 
+ * basis. Multiple threads do not overwrite each other's last-error code.
+ *
+ * @return The calling thread's last-error code.
+ */
+DWORD M2GetLastErrorKnownFailedCall()
+{
+    DWORD LastError = GetLastError();
+    return (LastError != ERROR_SUCCESS) ? LastError : ERROR_FUNCTION_FAILED;
+}
+
+/**
+ * Retrieves the calling thread's last-error code value if you can be sure that
+ * the last call was failed. The last-error code is maintained on a per-thread
+ * basis. Multiple threads do not overwrite each other's last-error code.
+ *
+ * @return The calling thread's last-error code which is converted to an
+ *         HRESULT value.
+ */
+HRESULT M2GetLastHRESULTErrorKnownFailedCall()
+{
+    return __HRESULT_FROM_WIN32(M2GetLastErrorKnownFailedCall());
+}
+
+/**
+ * Retrieves the address of an exported function or variable from the specified
+ * dynamic-link library (DLL).
+ *
+ * @param lpProcAddress The address of the exported function or variable.
+ * @param hModule A handle to the DLL module that contains the function or
+ *                variable. The LoadLibrary, LoadLibraryEx, LoadPackagedLibrary
+ *                or GetModuleHandle function returns this handle. This
+ *                function does not retrieve addresses from modules that were
+ *                loaded using the LOAD_LIBRARY_AS_DATAFILE flag. For more
+ *                information, see LoadLibraryEx.
+ * @param lpProcName The function or variable name, or the function's ordinal
+ *                   value. If this parameter is an ordinal value, it must be
+ *                   in the low-order word; the high-order word must be zero.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ */
 HRESULT M2GetProcAddress(
     _Out_ FARPROC& lpProcAddress,
     _In_ HMODULE hModule,
     _In_ LPCSTR lpProcName)
 {
     lpProcAddress = GetProcAddress(hModule, lpProcName);
-    return (nullptr != lpProcAddress) ? S_OK : M2GetLastError();
+    return (!lpProcAddress) ? S_OK : M2GetLastHRESULTErrorKnownFailedCall();
 }
 
-/// <summary>
-/// Retrieves the path of the executable file of the current process.
-/// </summary>
-/// <returns>
-/// If the function succeeds, the return value is the path of the executable
-/// file of the current process. If the function fails, the return value is an
-/// empty string.
-/// </returns>
+/**
+ * Retrieves the path of the executable file of the current process.
+ *
+ * @return If the function succeeds, the return value is the path of the
+ *         executable file of the current process. If the function fails, the
+ *         return value is an empty string.
+ */
 std::wstring M2GetCurrentProcessModulePath()
 {
     std::wstring result(MAX_PATH, L'\0');
@@ -223,19 +229,17 @@ std::wstring M2GetCurrentProcessModulePath()
     return result;
 }
 
-/// <summary>
-/// Parses a command line string and returns an array of the command line
-/// arguments, along with a count of such arguments, in a way that is similar to
-/// the standard C run-time.
-/// </summary>
-/// <param name="CommandLine">
-/// A string that contains the full command line. If this parameter is an empty
-/// string the function returns an array with only one empty string.
-/// </param>
-/// <returns>
-/// The return value is an array of the command line arguments, along with a
-/// count of such arguments.
-/// </returns>
+/**
+ * Parses a command line string and returns an array of the command line
+ * arguments, along with a count of such arguments, in a way that is similar to
+ * the standard C run-time.
+ *
+ * @param CommandLine A string that contains the full command line. If this
+ *                    parameter is an empty string the function returns an
+ *                    array with only one empty string.
+ * @return An array of the command line arguments, along with a count of such
+ *         arguments.
+ */
 std::vector<std::wstring> M2SpiltCommandLine(
     const std::wstring& CommandLine)
 {
@@ -368,28 +372,19 @@ std::vector<std::wstring> M2SpiltCommandLine(
     return SplitArguments;
 }
 
-/// <summary>
-/// Parses a command line string and get more friendly result.
-/// </summary>
-/// <param name="CommandLine">
-/// A string that contains the full command line. If this parameter is an empty
-/// string the function returns an array with only one empty string.
-/// </param>
-/// <param name="OptionPrefixes">
-/// One or more of the prefixes of option we want to use.
-/// </param>
-/// <param name="OptionParameterSeparators">
-/// One or more of the separators of option we want to use.
-/// </param>
-/// <param name="ApplicationName">
-/// The application name.
-/// </param>
-/// <param name="OptionsAndParameters">
-/// The options and parameters.
-/// </param>
-/// <param name="UnresolvedCommandLine">
-/// The unresolved command line.
-/// </param>
+/**
+ * Parses a command line string and get more friendly result.
+ *
+ * @param CommandLine A string that contains the full command line. If this
+ *                    parameter is an empty string the function returns an
+ *                    array with only one empty string.
+ * @param OptionPrefixes One or more of the prefixes of option we want to use.
+ * @param OptionParameterSeparators One or more of the separators of option we
+ *                                  want to use.
+ * @param ApplicationName The application name.
+ * @param OptionsAndParameters The options and parameters.
+ * @param UnresolvedCommandLine The unresolved command line.
+ */
 void M2SpiltCommandLineEx(
     const std::wstring& CommandLine,
     const std::vector<std::wstring>& OptionPrefixes,
@@ -477,6 +472,12 @@ void M2SpiltCommandLineEx(
                 // first space by adding 1 to the result.
                 wchar_t* command = wcsstr(search_start, L" ") + 1;
 
+                // Omit the space. (Thanks to wzzw.)
+                while (*command == L' ')
+                {
+                    ++command;
+                }
+
                 // Save
                 UnresolvedCommandLine = command;
 
@@ -486,4 +487,248 @@ void M2SpiltCommandLineEx(
     }
 }
 
-#endif // _M2_BASE_HELPERS_
+/**
+ * Retrieves file system attributes for a specified file or directory.
+ *
+ * @param FileHandle A handle to the file that contains the information to be
+ *                   retrieved. This handle should not be a pipe handle.
+ * @param FileAttributes The attributes of the specified file or directory.
+ *                       For a list of attribute values and their descriptions,
+ *                       see File Attribute Constants. If the function fails,
+ *                       the return value is INVALID_FILE_ATTRIBUTES.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ */
+HRESULT M2GetFileAttributes(
+    _In_ HANDLE FileHandle,
+    _Out_ PDWORD FileAttributes)
+{
+    FILE_BASIC_INFO BasicInfo;
+
+    if (GetFileInformationByHandleEx(
+        FileHandle,
+        FileBasicInfo,
+        &BasicInfo,
+        sizeof(FILE_BASIC_INFO)))
+    {
+        *FileAttributes = BasicInfo.FileAttributes;
+        return S_OK;
+    }
+    else
+    {
+        *FileAttributes = INVALID_FILE_ATTRIBUTES;
+        return M2GetLastHRESULTErrorKnownFailedCall();
+    }
+}
+
+/**
+ * Sets the attributes for a file or directory.
+ *
+ * @param FileHandle A handle to the file for which to change information. This
+ *                   handle must be opened with the appropriate permissions for
+ *                   the requested change. This handle should not be a pipe
+ *                   handle.
+ * @param FileAttributes The file attributes to set for the file. This
+ *                       parameter can be one or more values, combined using
+ *                       the bitwise - OR operator. However, all other values
+ *                       override FILE_ATTRIBUTE_NORMAL. For more information,
+ *                       see the SetFileAttributes function.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ */
+HRESULT M2SetFileAttributes(
+    _In_ HANDLE FileHandle,
+    _In_ DWORD FileAttributes)
+{
+    FILE_BASIC_INFO BasicInfo = { 0 };
+    BasicInfo.FileAttributes =
+        FileAttributes & (
+            FILE_SHARE_READ |
+            FILE_SHARE_WRITE |
+            FILE_SHARE_DELETE |
+            FILE_ATTRIBUTE_ARCHIVE |
+            FILE_ATTRIBUTE_TEMPORARY |
+            FILE_ATTRIBUTE_OFFLINE |
+            FILE_ATTRIBUTE_NOT_CONTENT_INDEXED |
+            FILE_ATTRIBUTE_NO_SCRUB_DATA) |
+        FILE_ATTRIBUTE_NORMAL;
+
+    if (SetFileInformationByHandle(
+        FileHandle,
+        FileBasicInfo,
+        &BasicInfo,
+        sizeof(FILE_BASIC_INFO)))
+    {
+        return S_OK;
+    }
+    else
+    {
+        return M2GetLastHRESULTErrorKnownFailedCall();
+    }
+}
+
+/**
+ * Deletes an existing file.
+ *
+ * @param FileHandle The handle of the file to be deleted.. This handle must be
+ *                   opened with the appropriate permissions for the requested
+ *                   change. This handle should not be a pipe handle.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark The way to get a file handle for this operation:
+ *         HANDLE hFile = CreateFileW(
+ *             lpFileName,
+ *             SYNCHRONIZE | DELETE | FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES,
+ *             FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+ *             nullptr,
+ *             OPEN_EXISTING,
+ *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+ *             nullptr);
+ */
+HRESULT M2DeleteFile(
+    _In_ HANDLE FileHandle)
+{
+    FILE_DISPOSITION_INFO DispostionInfo;
+    DispostionInfo.DeleteFile = TRUE;
+
+    if (SetFileInformationByHandle(
+        FileHandle,
+        FileDispositionInfo,
+        &DispostionInfo,
+        sizeof(FILE_DISPOSITION_INFO)))
+    {
+        return S_OK;
+    }
+    else
+    {
+        return M2GetLastHRESULTErrorKnownFailedCall();
+    }
+}
+
+/**
+ * Deletes an existing file, even the file have the readonly attribute.
+ *
+ * @param FileHandle The handle of the file to be deleted.. This handle must be
+ *                   opened with the appropriate permissions for the requested
+ *                   change. This handle should not be a pipe handle.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark The way to get a file handle for this operation:
+ *         HANDLE hFile = CreateFileW(
+ *             lpFileName,
+ *             SYNCHRONIZE | DELETE | FILE_READ_ATTRIBUTES | FILE_WRITE_ATTRIBUTES,
+ *             FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+ *             nullptr,
+ *             OPEN_EXISTING,
+ *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+ *             nullptr);
+ */
+HRESULT M2DeleteFileIgnoreReadonlyAttribute(
+    _In_ HANDLE FileHandle)
+{
+    HRESULT hr = S_OK;
+    DWORD OldAttribute = 0;
+
+    // Save old attributes.
+    hr = M2GetFileAttributes(
+        FileHandle,
+        &OldAttribute);
+    if (!SUCCEEDED(hr)) return hr;
+
+    // Remove readonly attribute.
+    hr = M2SetFileAttributes(
+        FileHandle,
+        OldAttribute & (-1 ^ FILE_ATTRIBUTE_READONLY));
+    if (!SUCCEEDED(hr)) return hr;
+
+    // Delete the file.
+    hr = M2DeleteFile(FileHandle);
+    if (!SUCCEEDED(hr))
+    {
+        // Restore attributes if failed.
+        hr = M2SetFileAttributes(
+            FileHandle,
+            OldAttribute);
+    }
+
+    return hr;
+}
+
+/**
+ * Retrieves the amount of space that is allocated for the file.
+ *
+ * @param FileHandle A handle to the file that contains the information to be
+ *                   retrieved. This handle should not be a pipe handle.
+ * @param AllocationSize A pointer to a ULONGLONG value that receives the
+ *                       amount of space that is allocated for the file, in
+ *                       bytes.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark The way to get a file handle for this operation:
+ *         HANDLE hFile = CreateFileW(
+ *             lpFileName,
+ *             GENERIC_READ | SYNCHRONIZE,
+ *             FILE_SHARE_READ,
+ *             nullptr,
+ *             OPEN_EXISTING,
+ *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+ *             nullptr);
+ */
+HRESULT M2GetFileAllocationSize(
+    _In_ HANDLE FileHandle,
+    _Out_ PULONGLONG AllocationSize)
+{
+    FILE_STANDARD_INFO StandardInfo;
+
+    if (GetFileInformationByHandleEx(
+        FileHandle,
+        FileStandardInfo,
+        &StandardInfo,
+        sizeof(FILE_STANDARD_INFO)))
+    {
+        *AllocationSize = static_cast<ULONGLONG>(
+            StandardInfo.AllocationSize.QuadPart);
+        return S_OK;
+    }
+    else
+    {
+        *AllocationSize = 0;
+        return M2GetLastHRESULTErrorKnownFailedCall();
+    }
+}
+
+/**
+ * Retrieves the size of the specified file.
+ *
+ * @param FileHandle A handle to the file that contains the information to be
+ *                   retrieved. This handle should not be a pipe handle.
+ * @param FileSize A pointer to a ULONGLONG value that receives the file size,
+ *                 in bytes.
+ * @return HRESULT. If the function succeeds, the return value is S_OK.
+ * @remark The way to get a file handle for this operation:
+ *         HANDLE hFile = CreateFileW(
+ *             lpFileName,
+ *             GENERIC_READ | SYNCHRONIZE,
+ *             FILE_SHARE_READ,
+ *             nullptr,
+ *             OPEN_EXISTING,
+ *             FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OPEN_REPARSE_POINT,
+ *             nullptr);
+ */
+HRESULT M2GetFileSize(
+    _In_ HANDLE FileHandle,
+    _Out_ PULONGLONG FileSize)
+{
+    FILE_STANDARD_INFO StandardInfo;
+
+    if (GetFileInformationByHandleEx(
+        FileHandle,
+        FileStandardInfo,
+        &StandardInfo,
+        sizeof(FILE_STANDARD_INFO)))
+    {
+        *FileSize = static_cast<ULONGLONG>(
+            StandardInfo.EndOfFile.QuadPart);
+        return S_OK;
+    }
+    else
+    {
+        *FileSize = 0;
+        return M2GetLastHRESULTErrorKnownFailedCall();
+    }
+}
